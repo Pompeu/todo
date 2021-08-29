332d50ca66789fdae6c430fcde69743cd6c32309
Jose Carlos <pompeulimp@gmail.com>
2021-08-29 18:31:13 -0300

    fix port and mongo name

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
-const koa =  require('koa');
-const app = module.exports = koa();
-const mongoose = require('mongoose');
-const serve = require('koa-static');
+const koa = require("koa");
+const app = (module.exports = koa());
+const mongoose = require("mongoose");
+const serve = require("koa-static");
 
-mongoose.connect('mongodb://localhost/todo');
-app.use(serve('public'));
+mongoose.connect("mongodb://localhost/mongoose");
+app.use(serve("public"));
 
-app.use(require('./routes/todo'));
-app.use(require('./routes/todoLocals'));
+app.use(require("./routes/todo"));
+app.use(require("./routes/todoLocals"));
 
-app.listen(5000);
+app.listen(3000);
